9db625e56e8df01f0a15eee6813b561cf1863d75
Johan <Johan@Notebook-JMAG.lan>
2016-03-07 18:46:46 +0100

    test

---
+++ OntwikkelStraatTest/src/test/java/OntwikkelStraatJUnitTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Johan
+ */
+public class OntwikkelStraatJUnitTest {
+    
+    public OntwikkelStraatJUnitTest() {
+    }
+    
+     @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void test() {
+        TestClass test = new TestClass();
+         Assert.assertEquals("1", test.doIets(1));
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    private static class TestClass {
+
+        public TestClass() {
+            
+        }
+
+        public String doIets(int i) {
+           return "1";
+        }
+    }    
+}
